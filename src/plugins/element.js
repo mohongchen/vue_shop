0eda3348ebcb9698dab93b6317397ce4cd75522b
mohongchen <3271057309@qq.com>
2019-11-20 10:02:19 +0800

    aaa

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,61 @@
 import Vue from 'vue'
+<<<<<<< HEAD
 import { Button } from 'element-ui'
 
 Vue.use(Button)
+=======
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(TableColumn)
+Vue.use(Table)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+// 挂载到原型对象上
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
+>>>>>>> 287a50d... 用户添加删除修改
